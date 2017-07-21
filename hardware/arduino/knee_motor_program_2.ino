a038fa6f5e9fab43bf216d5c9e85603a8ecf83cd
eajung <eriikjung@gmail.com>
2017-07-21 15:21:40 -0700

    Added a 3 state memory system for knee flexion

--- hardware/arduino/knee_motor_program_2.ino
+++ hardware/arduino/knee_motor_program_2.ino
@@ -16,6 +16,8 @@
 #include <Adafruit_MotorShield.h>
 #include <stdio.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
+
+
 // ------------------------------------------------------
 
 /*
@@ -23,6 +25,10 @@
  */
 #define FORCERESISTOR 10000 // value of the resistor in the circuit for calculating force
 #define THERMISTORPIN A0 // the pin to connect the sensor 
+#define STARTPOSITION = 0
+#define PEAKFLEXIONSTATE 1
+#define UNFLEXSTATE = 2
+
 
 const int button_pin = 2; // the number of the switch pin
 int button_state = 0; // this will change based on the state of the push button status
@@ -31,9 +37,12 @@ uint8_t i;
 unsigned long time; // time might be used by the delay()
 float fact; // dependent on the voltage 
 float stretchcord_reading; // analog reading for the stretch sensor
-float maximum_tension = 3.66; // software set maximum tension in cables compared to the stretchcord_reading 
-float taut_tension = 1.0; // software set tension for when the leg is back to being taught
+float maximum_tension = 2.7; // software set maximum tension in cables compared to the stretchcord_reading
+float fully_flexed_angle_tension = 2.2
+float taut_tension = 1.4; // software set tension for when the leg is back to being taught
 float force; // force calcuation based off of the stretchcord_reading
+
+int counter = 0;
 
 /* 
  * Create the motor shield object with the default I2C address
@@ -69,10 +78,15 @@ void decrease_motor_speed() {
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("KNEE Motion Program!");
+//  Serial.println("KNEE Motion Program!");
+
+// initialize control over the keyboard:
+//  Keyboard.begin();
   
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+
+  
  
   my_motor->setSpeed(0); // Set the speed to start, from 0 (off) to 255 (max speed)
   my_motor->run(FORWARD);
@@ -90,43 +104,59 @@ void loop() {
   delay(10); //refreashing time constant
   
   button_state = digitalRead(button_pin); // read the state of the switch value
-  Serial.println(button_state);
+  //  Serial.println(button_state);
   
   if (button_state == HIGH) {
-    Serial.print("Knee Flexion\n");
+  //    Serial.print("Knee Flexion\n");
     my_motor->run(FORWARD);
     increase_motor_speed();
   }
   
   stretchcord_reading = analogRead(THERMISTORPIN); // reads the A0 pin for the voltage values of the stretch cord sensor
-  Serial.print("Analog reading:");
-  Serial.println(stretchcord_reading);
+  //  Serial.print("Analog reading:");
+  //  Serial.println(stretchcord_reading);
  
   // convert the value to resistance
   stretchcord_reading = (1023 / stretchcord_reading)  - 1;
   stretchcord_reading = FORCERESISTOR / stretchcord_reading;
   
   force = ((0.759096*exp(0.00178499*stretchcord_reading)) - 1.26725); // formula found from sensor calibration tests
-  Serial.print("Force: ");
-  Serial.println(force);
+  //  Serial.print("Force: ");
+  //  Serial.println(force);
+
+  /*
+   * This allows us to mark when we have reached a specific angle.
+  char inChar;
+  //user hits enter 
+  if(Serial.available()){
+    inChar = Serial.read(); // Read the input from the send on the serial monitr
+    if(inChar == '\n'){
+      counter += 10;
+      Serial.print("angle is: ");
+      Serial.println(counter); // Tells us which angle we're at
+      Serial.println(force); // Tell us the force at the specified angle
+    }
+  }
+  */
   
-  if(force > maximum_tension) {
-    decrease_motor_speed();
-    Serial.println("Hold time");
-    delay(1000); // not sure if blocking code is the best..
-    Serial.println("Backwards release");
-    my_motor->run(BACKWARD);
-    increase_motor_speed();
-    flexed_state = 1;
+  if (force > maximum_tension) { // Let us know we've reached about 60-70 degree knee flexion
+    flexed_state = 1; 
   }
 
-  if((force < taut_tension) && (flexed_state == 1)) {
+  if ((force < fully_flexed_angle_tension) && (flexed_state == 1)){ // Approaching maximum degree for knee flexion
+    decrease_motor_speed();
+    delay(1000);
+    my_motor->run(BACKWARD);
+    increase_motor_speed();
+    flexed_state = 2;
+  }
+
+  if ((force < taut_tension) && (flexed_state == 2)) { // Stop motors once at rest length.
     decrease_motor_speed();
     Serial.println("Finished.");
     my_motor->run(RELEASE);
     delay(500);
-    flexed_state = 0;
-    exit(1);
+    flexed_state = STARTPOSITION;
   }
 }
 

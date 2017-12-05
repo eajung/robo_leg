73403cf1f79c486e97db74a1c280a0f038e30ad8
eajung <eriikjung@gmail.com>
2017-12-05 15:17:27 -0800

    Added file for tracking two Gyros

---
+++ hardware/arduino/REV_4/two_gyro_test.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+// The two BNO055 modules, bnoB has the ADR pin wired to 3.3v to change its i2c address
+// Both are wired: SCL to analog 5, SDA to analog 4, VIN to 5v, GRN to ground
+Adafruit_BNO055 bnoA = Adafruit_BNO055(-1, BNO055_ADDRESS_A);
+Adafruit_BNO055 bnoB = Adafruit_BNO055(-1, BNO055_ADDRESS_B);
+
+void setup() {
+   Serial.begin(115200);
+   if(!bnoA.begin()) {
+       Serial.print("Ooops, BNO055(A) not detected");
+       while(1);
+   }
+   bnoA.setExtCrystalUse(true);
+   if(!bnoB.begin()) {
+       Serial.print("Ooops, BNO055(B) not detected");
+       while(1);
+   }
+   bnoB.setExtCrystalUse(true);
+}
+
+void loop() {
+   imu::Vector<3> euler = bnoA.getVector(Adafruit_BNO055::VECTOR_EULER);
+   Serial.print(euler.x()); Serial.print(", ");
+   euler = bnoB.getVector(Adafruit_BNO055::VECTOR_EULER);
+   Serial.println(euler.x());
+}

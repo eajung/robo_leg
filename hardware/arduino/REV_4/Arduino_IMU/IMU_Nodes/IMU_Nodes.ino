3f53574cfa89a9c31e35d612221e1226f68df0fe
eajung <eriikjung@gmail.com>
2018-02-26 16:44:16 -0800

    Added modified IMU with dw1000

--- hardware/arduino/REV_4/Arduino_IMU/IMU_Nodes/IMU_Nodes.ino
+++ hardware/arduino/REV_4/Arduino_IMU/IMU_Nodes/IMU_Nodes.ino
@@ -38,7 +38,7 @@ boolean protocolFailed = false;
 #define LEN_DATA 16
 byte data[LEN_DATA];
 
-int addr = 3;
+int addr = 1;
 int dev_number = 5;
 // watchdog and reset period
 unsigned long lastActivity;

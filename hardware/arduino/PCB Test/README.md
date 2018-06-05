f3cf8b62593f9832c3314c8c80985dae52a821e6
eajung <eriikjung@gmail.com>
2018-06-05 16:52:49 -0700

    Added Keras build script and MPU example files

---
+++ hardware/arduino/PCB Test/README.md
@@ -0,0 +1,10 @@
+## Dependencies
+In order to run our software smoothly, you will need to add additional libraries to your Arduino IDE.
+
+## Install board managers:
+
+* [Adafruit ESP8266 Breakout](https://learn.adafruit.com/adafruit-huzzah-esp8266-breakout/using-arduino-ide) 
+
+## Install libraries
+
+* Put the MPU9250 Master folder into Documents/Arduino/libraries/
--- hardware/machine_learning/Keras_Build/keras_setup_script.sh
+++ hardware/machine_learning/Keras_Build/keras_setup_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Might need:
+# sudo pip3 uninstall numpy
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo | "\n"
 brew install python3
@@ -7,10 +10,13 @@ python3 -V
 sudo python3 get-pip.py
 curl 'https://bootstrap.pypa.io/get-pip.py' > get-pip.py && sudo python get-pip.py
 pip3 -V
-sudo easy_install --upgrade pip
-sudo easy_install nose
-sudo easy_install tornado
 sudo pip3 install --upgrade --ignore-installed --install-option '--install-data=/usr/local' numpy
 sudo pip3 install --ignore-install six
+sudo easy_install --upgrade pip
+sudo easy_install mpmath
+sudo pip3 install networkx
+sudo easy_install nose
+sudo easy_install tornado
 sudo pip3 install tensorflow
+sudo pip3 install --ignore-install keras
 Python3 Keras_Example.py

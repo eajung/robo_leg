3a87ed88ee580133e469b2a5978deff2e17a5318
Shreya <shreyak@eduroam-169-233-212-40.ucsc.edu>
2018-06-12 10:25:51 -0700

    Commented Motor Code

--- hardware/arduino/testing_harness/Reverse_DC_Motor/Reverse_DC_Motor.ino
+++ hardware/arduino/testing_harness/Reverse_DC_Motor/Reverse_DC_Motor.ino
@@ -17,35 +17,37 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) { // if serial input is ready to read input
-  input = Serial.readString();
-  command = input[0];
+  input = Serial.readString();  //input is user input
+  command = input[0]; //index is to take the first letter of user input in serial 
+  
+  //depending on whether the user put 's' 'x' or 'r', the code switches between the three cases
   switch(command) {
     case 's': // s for start
       Serial.print("Starting leg motion");
-      myMotor->setSpeed(150);
-      myMotor->run(FORWARD);
+      myMotor->setSpeed(150);  //set Speed to 150
+     
 
       uint8_t i;
-      myMotor->run(FORWARD);
+      myMotor->run(FORWARD); //set myMotor to move forward
 
       for (i=0; i<255; i++) {
-        myMotor->setSpeed(i);  
-        delay(10);
+        myMotor->setSpeed(i);  //incrementing the speed from 0-254
+        delay(10);    //after each increment, the motor is delayed by 10 milliseconds
       }
       break;
         
     case 'x' : // x for exit
       Serial.print("Exiting leg motion");
-      myMotor -> setSpeed(0);  
+      myMotor -> setSpeed(0);  //Setting speed to 0 to make the motor stop
       break;
         
 
     case 'r' : // r for reverse  
       Serial.print("Reversing leg motion");
-      myMotor->setSpeed(150);
-      myMotor->run(BACKWARD);
+      myMotor->run(BACKWARD); //just change direction of the motor spinning
       break;
-        
+
+  //nothing should change if user doesn't input anything      
     default:
       break;
       

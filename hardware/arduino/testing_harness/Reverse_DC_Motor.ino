47ebefa3c7eddcf3e507b4c94df3ac97fc05bb97
eajung <eriikjung@gmail.com>
2018-06-12 10:00:53 -0700

    Simplified code for testing harness

--- hardware/arduino/testing_harness/Reverse_DC_Motor.ino
+++ hardware/arduino/testing_harness/Reverse_DC_Motor.ino
@@ -10,12 +10,8 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); 
   AFMS.begin();  // create with the default frequency 1.6KHz
-  // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotor->setSpeed(150);
-  myMotor->run(FORWARD);
-  // turn on motor
-  myMotor->run(RELEASE);
 
+  myMotor->run(RELEASE); // turn on motor
 }
 
 void loop() {
@@ -24,87 +20,32 @@ void loop() {
   input = Serial.readString();
   command = input[0];
   switch(command) {
-    
-  case 's': // s for start
-  Serial.print("Starting leg motion");
-  myMotor->setSpeed(150);
-  myMotor->run(FORWARD);
-  // turn on motor
-  myMotor->run(RELEASE);
+    case 's': // s for start
+      Serial.print("Starting leg motion");
+      myMotor->setSpeed(150);
+      myMotor->run(FORWARD);
 
-  uint8_t i;
-  myMotor->run(FORWARD);
-  for (i=0; i<255; i++) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  
+      uint8_t i;
+      myMotor->run(FORWARD);
 
-  myMotor->run(BACKWARD);
-  for (i=0; i<255; i++) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
+      for (i=0; i<255; i++) {
+        myMotor->setSpeed(i);  
+        delay(10);
+      }
+      break;
+        
+    case 'x' : // x for exit
+      Serial.print("Exiting leg motion");
+      myMotor -> setSpeed(0);  
+      break;
+        
 
-  myMotor->run(RELEASE);
-  delay(1000);
-
-  break;
-      
-  case 'x' : // x for exit
-  Serial.print("Exiting leg motion");
-  myMotor -> setSpeed(0);
-    
-  break;
-      
-
-  case 'r' : // r for reverse  
-  Serial.print("Reversing leg motion");
-  myMotor->setSpeed(150);
-  myMotor->run(BACKWARD);
-  // turn on motor
-  myMotor->run(RELEASE);
-
-  while (true) {
-  myMotor->run(BACKWARD);
-  for (i=0; i<255; i--) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  
-  for (i=255; i!=0; i++) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  
-
-  myMotor->run(FORWARD);
-  for (i=0; i<255; i++) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-
-  myMotor->run(RELEASE);
-  delay(1000);
-  
-  
-  }
-
-  break;
-      
+    case 'r' : // r for reverse  
+      Serial.print("Reversing leg motion");
+      myMotor->setSpeed(150);
+      myMotor->run(BACKWARD);
+      break;
+        
     default:
       break;
       

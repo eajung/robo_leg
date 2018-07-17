3d8f37803bb6f82d38bbb2950f76e6a8b3432db1
eajung <eriikjung@gmail.com>
2018-07-16 17:25:04 -0700

    Wireless control compiled for testing

--- hardware/arduino/REV_5/REV_5_wireless_open_loop/command_REV_5.py
+++ hardware/arduino/REV_5/REV_5_wireless_open_loop/command_REV_5.py
@@ -1,30 +1,7 @@
 import socket
 import time
 
-###UDP_IP = "192.168.1.124"
-##UDP_IP = "192.168.1.107"
 UDP_PORT = 4210
-##
-##
-##print "UDP target IP:", UDP_IP
-##print "UDP target port:", UDP_PORT
-###print "message:", MESSAGE
-
-
-
-##MESSAGE = "1,200"
-##sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-##
-
-
-##for i in range(0,1024,100):
-##    
-##    MESSAGE = "1," + str(i)
-##    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-##    MESSAGE = "2," + str(1024-i)
-##    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-##    time.sleep(1)
-
 
 def send_command(command):
     # addr = "192.168.0." + str(110 + bot_id) # Ask Pat about how to authomatically search
